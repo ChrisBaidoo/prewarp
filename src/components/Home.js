07ba87cfc7b047706a104605793a9c8d257a919c
christian baidoo <chris_pens@hotmail.co.uk>
2020-07-27 16:22:44 +0100

    applied css

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,11 +9,19 @@ const Home = () => {
             <h1>Prewarp</h1>
           </div>
           <div className="col-md-6 col-sm-6 mb-30px">
-            <button className="btn btn-primary btn-sm m-2">About</button>
+            <div>
+              <ul className="buttons02 style1 buttons">
+                <li>
+                  <a href="#about" className="button n01">
+                    About
+                  </a>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
 
-        <div className="row">
+        <div className="row" id="about">
           <div className="col-md-6 col-sm-6 mb-30px">
             <h4>Launch product.</h4>
             <h4>Capture demand.</h4>
@@ -38,7 +46,7 @@ const Home = () => {
       <div className="row">
         <div className="col-md-6 col-sm-6 mb-30px">
           <div>
-            <img src="./src/tom-barber.jpeg" alt="Tom Barber"></img>
+            <img src="/src/tom-barber.jpeg" alt="Tom Barber"></img>
             <h6>Tom Barber</h6>
             <p>CEO and Co-founder</p>
           </div>
@@ -48,7 +56,14 @@ const Home = () => {
           </div>
         </div>
         <div class="col-md-6 col-sm-6">
-          <p>In private beta. For information:</p>
+          <p>
+            Copyright 2020 Prewarp Limited Companies House No. 12345678 All
+            rights reserved Privacy Policy
+          </p>
+          <p>
+            Lincoln Studios, The Biscuit Factory, Drummond Rd London SE16 4DG
+            hello@prewarp.co
+          </p>
         </div>
       </div>
     </div>
